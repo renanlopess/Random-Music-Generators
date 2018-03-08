764939fc9f34a14cc14d3d6de4440cc4e8a35ad8
Dave Cohen <dcohen18@gmail.com>
2018-03-08 14:35:00 -0600

    Add form UI

--- client/components/Melody.js
+++ client/components/Melody.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Form, Select } from 'semantic-ui-react';
+import { Form, Select, Button } from 'semantic-ui-react';
 
 /**
  * COMPONENT
@@ -11,15 +11,19 @@ export class Melody extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      formValues: { name: '', email: '', tonality: 'diatonic' }
+      formValues: { tonality: 'diatonic', midiRange: 'G3 G5', midiJump: 7, lengthValues: 4, midiLength: 10, midiQTY: 10 }
     };
   }
 
-  handleChange = (evt, { name, value }) => this.setState({formValues: { [name]: value }})
+  handleChange = (evt, { name, value }) => {
+    const {formValues} = this.state;
+    formValues[name] = value;
+    this.setState({formValues});
+  }
 
   handleSubmit = (evt) => {
     evt.preventDefault();
-    const { name, email, tonality } = this.state;
+    const { tonality, midiRange, midiJump, lengthValues, midiLength, midiQTY } = this.state.formValues;
     alert('submitted');
 
     // this.setState({ submittedName: name, submittedEmail: email });
@@ -27,10 +31,22 @@ export class Melody extends Component {
 
   render() {
     // const { email } = this.props;
-    const { name, email, tonality } = this.state;
+    const { tonality, midiRange, midiJump, lengthValues, midiLength, midiQTY } = this.state.formValues;
     const tonalityOptions = [
       { key: 'diatonic', text: 'Diatonic', value: 'diatonic' },
       { key: 'chromatic', text: 'Chromatic', value: 'chromatic' },
+    ];
+    const lengthValueOptions = [
+      { key: 1, text: 'Whole Note (4 beats)', value: 1 },
+      { key: 2, text: 'Half Note (2 beats)', value: 2 },
+      { key: 4, text: 'Quarter Note (1 beat)', value: 4 },
+      { key: 6, text: 'Quarter Note Triplet (2/3 beat)', value: 6 },
+      { key: 8, text: 'Eighth Note (1/2 beat)', value: 8 },
+      { key: 12, text: 'Eighth note Triplet (1/3 beat)', value: 12 },
+      { key: 16, text: 'Sixteenth Note (1/4 beat)', value: 16 },
+      { key: 24, text: 'Sixteenth Note Triplet (1/6 beat)', value: 24 },
+      { key: 32, text: 'Thirty-second Note (1/8 beat)', value: 32 },
+      { key: 64, text: 'Sixty-fourth Note (1/16 beat)', value: 64 }
     ];
     return (
       <div>
@@ -38,11 +54,17 @@ export class Melody extends Component {
         <div>
         <Form onSubmit={this.handleSubmit}>
           <Form.Group>
-            <Form.Input label="name" placeholder="Name" name="name" value={name} onChange={this.handleChange} />
-            <Form.Input label="email" placeholder="Email" name="email" value={email} onChange={this.handleChange} />
-            <Form.Field control={Select} label="Tonality Options" options={tonalityOptions} placeholder="hey" name="tonality" value={tonality} onChange={this.handleChange} />
-            <Form.Button content="Submit" />
+          <Form.Field control={Select} label="Tonality Options" options={tonalityOptions} defaultValue="diatonic" name="tonality" value={tonality} onChange={this.handleChange} />          
+          <Form.Field size="tiny" control={Select} label="Note Durations" options={lengthValueOptions} defaultValue="Quarter Note (1 beat)" name="lengthValues" value={lengthValues} onChange={this.handleChange} />
           </Form.Group>
+
+          <Form.Group>
+          <Form.Input label="Note Range" name="midiRange" placeholder="G3 G5" value={midiRange} onChange={this.handleChange}/>
+          <Form.Input label="Max Jump" name="midiJump" value={midiJump} placeholder="Min: 0, Max: 127" onChange={this.handleChange}/>
+          <Form.Input type="number" label="No of Notes" name="midiLength" value={midiLength} placeholder="Min: 1, Max: 50" min={1} max= {50} onChange={this.handleChange}/>
+          <Form.Input type="number" label="No of Melodies" name="midiQTY" value={midiQTY} placeholder="Min: 1, Max: 20" min={1} max= {20} onChange={this.handleChange}/>
+          </Form.Group>
+          <Form.Button content="Submit" />
         </Form>
         <strong>state:</strong>
         <pre>{JSON.stringify(this.state, null, 2)}</pre>
@@ -63,51 +85,3 @@ const mapState = state => {
 };
 
 export default connect(mapState)(Melody);
-
-/*
-        <div>
-          <label htmlFor="tonalityOptions">Tonality</label>
-          <select id="tonalityOptions">
-            <option value="diatonic" selected>
-              Diatonic
-            </option>
-            <option value="chromatic">Chromatic</option>
-          </select>
-          <label htmlFor="midiRange">
-            <a href="refTable.html" target="_blank">
-              Note Range
-            </a>
-          </label>
-          <input id="midiRange" placeholder="G3 G5" value="G3 G5" />
-
-          <label htmlFor="midiJump">Max Jump</label>
-          <input type="number" id="midiJump" placeholder="12" value="12" />
-
-          <label htmlFor="lengthValues">
-            Note Durations
-            <select id="lengthValues">
-              <option value="1">Whole Note (4 beats)</option>
-              <option value="2">Half Note (2 beats)</option>
-              <option value="4" selected>
-                Quarter Note (1 beat)
-              </option>
-              <option value="6">Quarter Note Triplet (2/3 beat)</option>
-              <option value="8">Eighth Note (1/2 beat)</option>
-              <option value="12">Eighth Note Triplet (1/3 beat)</option>
-              <option value="16">Sixteenth Note (1/4 beat)</option>
-              <option value="24">Sixteenth Note Triplet (1/6 beat)</option>
-              <option value="32">Thirty-second Note (1/8 beat)</option>
-              <option value="64">Sixty-fourth Note (1/16 beat)</option>
-            </select>
-          </label>
-          <label htmlFor="midiLength">No of Notes</label>
-          <input type="number" id="midiLength" placeholder="10" value="10" />
-          <label htmlFor="midiQTY">No of Melodies</label>
-          <input type="number" id="midiQTY" placeholder="10" value="10" />
-          <br />
-          <input type="button" id="generate" value="Generate" />
-
-          <div id="results">RESULTS</div>
-        </div>
-
-*/
