2972b69f4f8f64469af86a2c5c88e44ec5ea45e3
Dave Cohen <dcohen18@gmail.com>
2018-03-09 11:11:34 -0600

    Made functions modular, Rename stuff

---
+++ script/generateMidiArray.js
@@ -0,0 +1,63 @@
+const MidiWriter = require('midi-writer-js');
+const createRandomMelody = require('./createRandomMelody');
+const {numToNote} = require('./noteUtilities');
+
+/*eslint-disable complexity*/
+// called when 'generate' is clicked
+function generateMidiArray({
+  tonality = 'diatonic',
+  pitchRange = '0 127',
+  midiLength = 10,
+  intervalJump = 7,
+  rhythmValues = 4,
+  midiQuantity = 2
+} = {}) {
+  let results = [];
+  for (let i = 0; i < midiQuantity; i++) {
+    let midiCodeArray = createRandomMelody({ tonality, pitchRange, midiLength, intervalJump });
+    let pitchNamesWithOctave = midiCodeArray.map(num => numToNote(num));
+    let midiFile = createMidiFile(midiCodeArray, rhythmValues);
+    results.push({
+      midiCodeArray, pitchNamesWithOctave, midiFile
+    });
+  }
+  return results;
+}
+
+function createMidiFile(midiCodeArray, rhythmValues) {
+  let track = new MidiWriter.Track();
+  let noteArray = [];
+  for (let j = 0; j < midiCodeArray.length; j++) {
+    let pitch = [midiCodeArray[j]];
+    let duration = rhythmValues || 2;
+    let note = new MidiWriter.NoteEvent({ pitch, duration });
+    noteArray.push(note);
+  }
+  track.addEvent(noteArray);
+  let write = new MidiWriter.Writer([track]);
+  let file = write.dataUri();
+  return file;
+}
+
+function getDownloadLink(url) {
+  return `<a class="download" title="Download" href="${url}">
+          <i class="fa fa-music fa-lg" aria-hidden="true"></i>
+          </a>`;
+}
+
+// let tonalityOptions = document.getElementById('tonalityOptions');
+// let midiRange = document.getElementById('midiRange');
+// let midiLength = document.getElementById('midiLength');
+// let midiJump = document.getElementById('midiJump');
+// let lengthValues = document.getElementById('lengthValues');
+// let midiQTY = document.getElementById('midiQTY');
+// let generate = document.getElementById('generate');
+// let results = document.getElementById('results');
+
+// generate.addEventListener('click', () => createMidi());
+
+module.exports = {
+  generateMidiArray,
+  getDownloadLink
+};
+
