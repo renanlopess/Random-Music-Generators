ff4c9a76bac2dae023cd3d617bffa8a0d3eb002f
Dave Cohen <dcohen18@gmail.com>
2018-03-08 20:58:38 -0600

    Update exports

--- client/components/index.js
+++ client/components/index.js
@@ -5,5 +5,7 @@
  */
 export {default as Navbar} from './navbar';
 export {default as UserHome} from './user-home';
-export {default as Home} from './Home';
 export {Login, Signup} from './auth-form';
+export {default as Home} from './Home';
+export {default as Melody} from './Melody';
+export {default as MelodyResult} from './MelodyResult';
