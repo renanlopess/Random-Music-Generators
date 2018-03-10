6ebece3e8680edbc1c1b2a751850d3bdc1fd8ef0
Dave Cohen <dcohen18@gmail.com>
2018-03-10 16:57:39 -0600

    Add main functionality

--- client/components/MelodyResult.js
+++ client/components/MelodyResult.js
@@ -1,40 +1,114 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Table } from 'semantic-ui-react';
+import { Table, Button, Icon, Modal } from 'semantic-ui-react';
+import Tone from 'Tone';
+import previewMelody from './PreviewMelody';
+import NoteGrid from './NoteGrid';
 
+// import testMusical from '../../script/testMusical';
 /**
  * COMPONENT
  */
 
-export const MelodyResult = (props) => {
-  const noteNumbers = [23, 24, 25];
-  const pitches = ['C', 'C#', 'D'];
-  console.log('PROPS in melodyResult:', props);
+export class MelodyResult extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalOpen: false
+    };
+  }
+  handleOpen = (midiDataObject) => {
+    this.setState({ modalOpen: true }, () => {
+      previewMelody(true, midiDataObject);
+    });
+  };
 
-  return (
-    <Table definition>
-      <Table.Body>
-        <Table.Row>
-          <Table.Cell>MIDI Note Number</Table.Cell>
-          {
-            noteNumbers.map((num, i) => {
-              return <Table.Cell key={i}>{num}</Table.Cell>;
-            })
-          }
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell>Pitch</Table.Cell>
-          {
-            pitches.map((num, i) => {
-              return <Table.Cell key={i}>{num}</Table.Cell>;
-            })
-          }
-        </Table.Row>
-      </Table.Body>
-    </Table>
-  );
-};
+  handleClose = () => {
+    previewMelody(false);
+    this.setState({ modalOpen: false });
+  }
+
+  createTableCells(arr) {
+    return arr.map((num, i) => {
+      return <Table.Cell key={i}>{num}</Table.Cell>;
+    });
+  }
+
+  playExampleTone = () => {
+    console.log('clicked');
+    // playTone();
+  };
+
+  render() {
+    const noteNumbers = [23, 24, 25];
+    const pitches = ['C', 'C#', 'D'];
+    console.log('PROPS/State in melodyResult:', this.props, this.state);
+
+    if (!this.props.melodies.length) {
+      return (
+        <div>
+          <h3>No results to display.</h3>
+        </div>
+      );
+    }
+
+    return (
+      <div style={{ width: '900px', overflowX: 'scroll' }}>
+        {this.props.melodies.map((midiDataObject, i) => {
+          return (
+            <div key={i}>
+              <Table definition key={i}>
+                <Table.Body>
+                  <Table.Row>
+                    <Table.Cell>Pitch</Table.Cell>
+                    {this.createTableCells(midiDataObject.pitchNamesWithOctave)}
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>MIDI Code</Table.Cell>
+                    {this.createTableCells(midiDataObject.midiCodeArray)}
+                  </Table.Row>
+                </Table.Body>
+              </Table>
+              <Modal
+                open={this.state.modalOpen}
+                onClose={this.handleClose}
+                trigger={
+                  <Button
+                    icon
+                    labelPosition="left"
+                    color="blue"
+                    onClick={() => this.handleOpen(midiDataObject)}
+                  >
+                    <Icon name="play" />
+                    PREVIEW
+                  </Button>
+                }
+                closeIcon
+              >
+                <Modal.Content>
+                  <NoteGrid melody={midiDataObject} />
+                </Modal.Content>
+              </Modal>
+
+              <a
+                className="download"
+                title="Download"
+                href={midiDataObject.midiFile}
+              >
+                <Button icon labelPosition="left" color="grey">
+                  <Icon name="download" />
+                  DOWNLOAD
+                </Button>
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
 /**
  * CONTAINER
  */
@@ -45,3 +119,8 @@ const mapState = state => {
 };
 
 export default connect(mapState)(MelodyResult);
+
+/*
+{createNoteNumbers(midiDataObject)}
+{createPitchNames(midiDataObject)}
+*/
---
+++ client/components/NoteGrid.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Melody from './Melody';
+import { Table } from 'semantic-ui-react';
+
+/**
+ * COMPONENT
+ */
+export const NoteGrid = props => {
+  const midiData = props.melody;
+  const rows = 2;
+  // const cols = 3;
+  const active = '2-0';
+  // const map = { 0: 2, 1: 1, 2: 0 };
+  const melody = midiData.pitchNamesWithOctave;
+
+  const buildTable = (rows, arr) => {
+    const cells = [];
+    for (let i = 0; i < rows; i++) {
+      cells.push(
+        <Table.Row key={i}>
+          {arr.map((item, j) => {
+            let cls = `${j}-${i}`;
+            if (cls === active) {
+              cls += ' active';
+            }
+            return (
+              <Table.Cell key={`${j}-${i}`} className={cls}>
+                {j}-{i}
+              </Table.Cell>
+            );
+          })}
+        </Table.Row>
+      );
+    }
+    return cells;
+  };
+
+  return (
+    <Table celled>
+      <Table.Body>{buildTable(rows, melody)}</Table.Body>
+    </Table>
+  );
+};
+
+/**
+ * CONTAINER
+ */
+const mapState = state => {
+  return {
+    // email: state.user.email
+  };
+};
+
+export default connect(mapState)(NoteGrid);
