152aaa003468016f217a34d5d55f26bc49ecebdf
Dave Cohen <dcohen18@gmail.com>
2018-03-11 13:42:58 -0500

    Convert to stateful component

--- client/components/PreviewMelody.js
+++ client/components/PreviewMelody.js
@@ -1,37 +1,108 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Table, Button, Icon, Modal } from 'semantic-ui-react';
 import Tone from 'Tone';
 
-const rhythm = '4n';
-const tempo = 160;
+/**
+ * COMPONENT
+ */
 
-export default function togglePlay(modalOpen, midiDataObj) {
-  const melodyArray = midiDataObj ? midiDataObj.pitchNamesWithOctave : [];
-  // SEQUENCE - have to trigger release on modal close or stop
-  var synth = new Tone.Synth().toMaster();
-  synth.volume.value = -6;
+export class PreviewMelody extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rhythm: '8n',
+      tempo: 80,
+      synth: new Tone.Synth().toMaster(),
+      sequence: null,
+      playing: false
+    };
+  }
 
-  //pass in an array of events
-  var part = new Tone.Sequence(
-    function(time, event) {
-      //the events will be given to the callback with the time they occur
-      synth.triggerAttackRelease(event);
-    },
-    melodyArray,
-    rhythm
-  );
-  if (modalOpen) {
-    //start the part at the beginning of the Transport's timeline
-    part.start(0);
+  componentDidMount() {
+    // console.log('cdm')
+    this.togglePlay(this.props.melody);
+  }
+  componentWillUnmount() {
+    // console.log('c will unmount')
+    this.togglePlay();
+  }
 
-    //loop the part 3 times (true for infinite)
-    part.loop = 2;
-    // part.loopEnd = '1m';//leave this off
+  togglePlay(midiData) {
+    // console.log('toggleplay params: midiData', midiData);
+    const melodyArray = midiData ? midiData.pitchNamesWithOctave : [];
+    if (!melodyArray.length) {
+      this.state.synth.triggerRelease();
+      if (this.state.sequence) {
+        this.state.sequence.stop();
+      }
+      Tone.Transport.stop();
+      this.setState(
+        {
+          sequence: null,
+          playing: false
+        },
+        () => {
+          console.log('modal closed, state:', this.state);
+        }
+      );
+      // synth.volume.value = -80;
+      return;
+    }
+    // SEQUENCE - have to trigger release on modal close or stop
+    var synth = this.state.synth;
+    synth.volume.value = -6;
 
-    Tone.Transport.bpm.value = tempo;
-    Tone.Transport.start('+0.1');
-  } else {
-    synth.triggerRelease();
-    Tone.Transport.stop();
-    // part.stop();
-    // synth.volume.value = -80;
+    //pass in an array of events
+    let seq = new Tone.Sequence(
+      function(time, event) {
+        //the events will be given to the callback with the time they occur
+        synth.triggerAttackRelease(event, this.state.rhythm);
+      },
+      melodyArray,
+      this.state.rhythm
+    );
+    //start the seq at the beginning of the Transport's timeline
+    seq.start(0);
+
+    //loop the seq 3 times (true for infinite)
+    seq.loop = 2;
+    this.setState({ sequence: seq, playing: true, synth }, () => {
+      // seq.loopEnd = '1m';//leave this off
+      Tone.Transport.bpm.value = this.state.tempo;
+      Tone.Transport.start('+0.1');
+      console.log('modal open, state:', this.state);
+    });
+    // var seq = new Tone.Sequence(
+    //   function(time, event) {
+    //     //the events will be given to the callback with the time they occur
+    //     synth.triggerAttackRelease(event, this.state.rhythm);
+    //   },
+    //   melodyArray,
+    //   this.state.rhythm
+    // );
+  }
+
+  render() {
+    const { rhythm, tempo, synth, sequence, transport, playing } = this.state;
+    // console.log('preview state/props:', this.state, this.props);
+    return <div />;
   }
 }
+
+/**
+ * CONTAINER
+ */
+const mapState = state => {
+  return {
+    melody: state.melody
+  };
+};
+
+export default connect(mapState)(PreviewMelody);
+
+/*
+{createNoteNumbers(midiDataObject)}
+{createPitchNames(midiDataObject)}
+*/
