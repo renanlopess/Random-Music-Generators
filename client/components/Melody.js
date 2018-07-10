43785c6f4e8696b066ff91cf086b84fab745656e
Dave Cohen <dcohen18@gmail.com>
2018-07-10 16:37:11 -0500

    Implement code review edits

--- client/components/Melody.js
+++ client/components/Melody.js
@@ -6,61 +6,59 @@ import MelodyResult from './MelodyResult';
 import {getMelodies} from '../store/melodies';
 import { generateMidiArray } from '../../script/generateMidiArray';
 
+const INITIAL_FORM_VALUES = {
+  tonality: 'diatonic',
+  pitchRange: 'G3 G5',
+  intervalJump: 7,
+  rhythmValues: 4,
+  midiLength: 10,
+  midiQuantity: 2
+};
+
 /**
- * COMPONENT
+ * PN: what about putting tonalityOptions and lengthValueOptions
+ * outside of the class? these values will get reinstantiated every time
+ * this component rerenders
  */
+const tonalityOptions = [
+  { key: 'diatonic', text: 'Diatonic', value: 'diatonic' },
+  { key: 'chromatic', text: 'Chromatic', value: 'chromatic' },
+];
+
+const lengthValueOptions = [
+  { key: 1, text: 'Whole Note (4 beats)', value: 1 },
+  { key: 2, text: 'Half Note (2 beats)', value: 2 },
+  { key: 4, text: 'Quarter Note (1 beat)', value: 4 },
+  { key: 6, text: 'Quarter Note Triplet (2/3 beat)', value: 6 },
+  { key: 8, text: 'Eighth Note (1/2 beat)', value: 8 },
+  { key: 12, text: 'Eighth note Triplet (1/3 beat)', value: 12 },
+  { key: 16, text: 'Sixteenth Note (1/4 beat)', value: 16 },
+  { key: 24, text: 'Sixteenth Note Triplet (1/6 beat)', value: 24 },
+  { key: 32, text: 'Thirty-second Note (1/8 beat)', value: 32 },
+  { key: 64, text: 'Sixty-fourth Note (1/16 beat)', value: 64 }
+];
 
 export class Melody extends Component {
   constructor(props) {
     super(props);
-    /** What about having the formValues at the state?
-     * this.state = {tonality: 'diatonic', ...}
-     */
-    this.state = {
-      formValues: { tonality: 'diatonic', pitchRange: 'G3 G5', intervalJump: 7, rhythmValues: 4, midiLength: 10, midiQuantity: 2 }
-    };
+    this.state = {...INITIAL_FORM_VALUES};
   }
 
   handleChange = (evt, { name, value }) => {
-    const {formValues} = this.state;
-    formValues[name] = value;
-    /** if tonality, pitchRange were in the state you could just
-     * this.setState({[name]: value})
-     */
-    this.setState({formValues});
+    this.setState({[name]: value});
   }
 
   handleSubmit = (evt) => {
     evt.preventDefault();
-    const midiArray = generateMidiArray(this.state.formValues);
+    const midiArray = generateMidiArray(this.state);
     this.props.getMelodies(midiArray);
   }
 
   render() {
     // const { email } = this.props;
-    // console.log('melody props',this.props)
-    const { tonality, pitchRange, intervalJump, rhythmValues, midiLength, midiQuantity } = this.state.formValues;
-    /**
-     * PN: what about putting tonalityOptions and lengthValueOptions
-     * outside of the class? these value will get reinstantiated every time
-     * this component rerenders
-     */
-    const tonalityOptions = [
-      { key: 'diatonic', text: 'Diatonic', value: 'diatonic' },
-      { key: 'chromatic', text: 'Chromatic', value: 'chromatic' },
-    ];
-    const lengthValueOptions = [
-      { key: 1, text: 'Whole Note (4 beats)', value: 1 },
-      { key: 2, text: 'Half Note (2 beats)', value: 2 },
-      { key: 4, text: 'Quarter Note (1 beat)', value: 4 },
-      { key: 6, text: 'Quarter Note Triplet (2/3 beat)', value: 6 },
-      { key: 8, text: 'Eighth Note (1/2 beat)', value: 8 },
-      { key: 12, text: 'Eighth note Triplet (1/3 beat)', value: 12 },
-      { key: 16, text: 'Sixteenth Note (1/4 beat)', value: 16 },
-      { key: 24, text: 'Sixteenth Note Triplet (1/6 beat)', value: 24 },
-      { key: 32, text: 'Thirty-second Note (1/8 beat)', value: 32 },
-      { key: 64, text: 'Sixty-fourth Note (1/16 beat)', value: 64 }
-    ];
+    // console.log('melody props', this.props, this.state);
+    const { tonality, pitchRange, intervalJump, rhythmValues, midiLength, midiQuantity } = this.state;
+
     return (
       <div className="melody-main-wrapper">
       <h3 style={{display: 'none'}}>Random Melody Generator</h3>
@@ -97,7 +95,6 @@ const mapState = state => {
 const mapDispatch = dispatch => {
   return {
     getMelodies(melodies) {
-      /** dispatch isn't defined */
       return dispatch(getMelodies(melodies));
     }
   };
