378a7e0409df00b34b1db38f890a273901e2b931
Dave Cohen <dcohen18@gmail.com>
2018-07-26 21:05:35 -0500

    Fix optional prop error on MelodyModal

---
+++ client/components/MelodyScene/MelodyModal.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Button, Icon, Modal } from 'semantic-ui-react';
+import PreviewMelody from './PreviewMelody';
+import { selectMelody, removeMelody } from '../../store/melody';
+
+/**
+ * COMPONENT
+ */
+
+export class MelodyModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalOpen: false,
+      fullscreen: false,
+    };
+  }
+  handleOpen = midiDataObject => {
+    Promise.resolve(this.props.selectMelody(midiDataObject)).then(() => {
+      const len = midiDataObject.midiCodeArray.length;
+      this.setState({ modalOpen: true, fullscreen: len > 14 });
+    });
+  };
+
+  handleClose = () => {
+    this.setState({ modalOpen: false }, () => {
+      this.props.removeMelody();
+    });
+  };
+
+  render() {
+    const modalSizeProp = {};
+    if (this.state.fullscreen) {
+      modalSizeProp.size = 'fullscreen';
+    }
+    return (
+      <Modal
+        open={this.state.modalOpen}
+        onClose={this.handleClose}
+        trigger={
+          <Button
+            icon
+            labelPosition="left"
+            color="blue"
+            onClick={() => this.handleOpen(this.props.midiDataObject)}
+            {...modalSizeProp}
+          >
+            <Icon name="play" />
+            PREVIEW
+          </Button>
+        }
+        closeIcon
+      >
+        <Modal.Content>
+          <PreviewMelody />
+        </Modal.Content>
+      </Modal>
+    );
+  }
+}
+
+/**
+ * CONTAINER
+ */
+const mapState = state => {
+  return {
+    melodies: state.melodies
+  };
+};
+
+const mapDispatch = dispatch => {
+  return {
+    selectMelody(melody) {
+      return dispatch(selectMelody(melody));
+    },
+    removeMelody() {
+      return dispatch(removeMelody());
+    }
+  };
+};
+
+export default connect(mapState, mapDispatch)(MelodyModal);
